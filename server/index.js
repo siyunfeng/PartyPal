b8dfe87ef3b49ce8cea00af4f86c9e563a7fb6b6
Jane Yeh <jane.yeh72@gmail.com>
2022-11-28 22:28:29 -0500

    rm duplicate port

--- server/index.js
+++ server/index.js
@@ -1,28 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const morgan = require("morgan");
-app.use(morgan("dev"));
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "../public")));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../public')));
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const { syncAndSeed } = require("./db/seed");
+const { syncAndSeed } = require('./db/seed');
 
-
-app.use("/api", require("./apiRoutes"));
-app.use("/auth", require("./apiRoutes/auth"));
+app.use('/api', require('./apiRoutes'));
+app.use('/auth', require('./apiRoutes/auth'));
 
 // if (process.env.NODE_ENV !== "production") require("../secrets")
 // const SECRET = process.env.SECRET
 
-app.get("*", (req, res, next) => {
+app.get('*', (req, res, next) => {
   try {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   } catch (error) {
     next(error);
   }
@@ -41,13 +40,13 @@ app.use((error, req, res, next) => {
   console.error(error.stack);
   res
     .status(error.status || 500)
-    .send(error.message || "Internal Server error");
+    .send(error.message || 'Internal Server error');
 });
 
 syncAndSeed();
 
-const port = process.env.PORT || 3019
+const port = process.env.PORT || 3019;
 
-app.listen(port, () => console.log(`listening on ${port} 3019`));
+app.listen(port, () => console.log(`listening on ${port}`));
 
 module.exports = app;
