c6307c93ff792703f99ee907799e696196bb6b59
Jane Yeh <jane.yeh72@gmail.com>
2022-11-28 22:25:54 -0500

    rm consolelog

--- server/apiRoutes/auth.js
+++ server/apiRoutes/auth.js
@@ -1,14 +1,14 @@
-const authRouter = require("express").Router();
-const { User } = require("../db/models/User");
+const authRouter = require('express').Router();
+const { User } = require('../db/models/User');
 
 authRouter.get('/me', async (req, res, next) => {
   try {
-    res.send(await User.findByToken(req.headers.authorization))
+    res.send(await User.findByToken(req.headers.authorization));
   } catch (ex) {
-    next(ex)
+    next(ex);
   }
-})
-console.log("hello")
+});
+
 authRouter.post(`/login`, async (req, res, next) => {
   try {
     res.send({ token: await User.authenticate(req.body) });
@@ -17,18 +17,17 @@ authRouter.post(`/login`, async (req, res, next) => {
   }
 });
 
-
-authRouter.post("/signup", async (req, res, next) => {
+authRouter.post('/signup', async (req, res, next) => {
   try {
-    const { username, password } = req.body; 
+    const { username, password } = req.body;
     // only getting username and password off req.body
     // so we only add to those fields in database
     // any routes recieving front end data
     const user = await User.create({ username, password });
     res.send({ token: await user.generateToken() });
   } catch (err) {
-    if (err.name === "SequelizeUniqueConstraintError") {
-      res.status(401).send("User already exists");
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists');
     } else {
       next(err);
     }
