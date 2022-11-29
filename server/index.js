9b11a17b405cbeb4a122dfec34b67210158204b4
Siyun Feng <siyunf21@gmail.com>
2022-11-29 01:12:04 -0500

    removed some duplicate code

--- server/index.js
+++ server/index.js
@@ -1,53 +1,45 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const morgan = require("morgan");
-app.use(morgan("dev"));
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "../public")));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../public')));
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const { syncAndSeed } = require("./db/seed");
+const { syncAndSeed } = require('./db/seed');
 
+app.use('/api', require('./apiRoutes'));
+app.use('/auth', require('./apiRoutes/auth'));
 
-app.use("/api", require("./apiRoutes"));
-app.use("/auth", require("./apiRoutes/auth"));
-
+// QUESTION: do we still need to keep this?
 // if (process.env.NODE_ENV !== "production") require("../secrets")
 // const SECRET = process.env.SECRET
 
-app.get("*", (req, res, next) => {
+app.get('*', (req, res, next) => {
   try {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
   } catch (error) {
     next(error);
   }
 });
-
-// app.use("*", (req, res, next) => {
-//   try {
-//     res.sendFile(path.join(__dirname, "../public/index.html"));
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 app.use((error, req, res, next) => {
   console.error(error);
   console.error(error.stack);
   res
     .status(error.status || 500)
-    .send(error.message || "Internal Server error");
+    .send(error.message || 'Internal Server error');
 });
 
 syncAndSeed();
 
-const port = process.env.PORT || 3019
+const port = process.env.PORT || 3019;
 
-app.listen(port, () => console.log(`listening on ${port} 3019`));
+app.listen(port, () => console.log(`listening on ${port}`));
 
 module.exports = app;
