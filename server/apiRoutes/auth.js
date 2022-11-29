7c8f353dfd42b1f91bc359bd0c8739b89fb0c8a9
Irais Valenzuela <iraisvq8@gmail.com>
2022-11-28 20:18:19 -0500

    idk what happened

--- server/apiRoutes/auth.js
+++ server/apiRoutes/auth.js
@@ -8,7 +8,7 @@ authRouter.get('/me', async (req, res, next) => {
     next(ex)
   }
 })
-
+console.log("hello")
 authRouter.post(`/login`, async (req, res, next) => {
   try {
     res.send({ token: await User.authenticate(req.body) });
