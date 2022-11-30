230bdc46c5914e22d438170786041ab6f84d1e93
Jane Yeh <jane.yeh72@gmail.com>
2022-11-30 17:35:58 -0500

    export reducer

--- client/redux/index.js
+++ client/redux/index.js
@@ -5,9 +5,11 @@ import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import auth from './auth';
+import allCatererReducer from './caterer';
 
 export const reducer = combineReducers({
   auth,
+  allCatererReducer,
   // other subreducers
 });
 
