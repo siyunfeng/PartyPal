b6fe5a60960af89ad98ad1a48a6d03caa3c86e71
Irais Valenzuela <iraisvq8@gmail.com>
2022-11-30 17:41:52 -0500

    can get single and all venues from axios call yippie

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,12 +1,14 @@
-const router = require("express").Router();
-const { userRouter } = require("./users");
+const router = require('express').Router();
+const { userRouter } = require('./users');
+const venuesRouter = require('./venues');
 
-router.use("/users", userRouter);
+router.use('/users', userRouter);
+router.use('/venues', venuesRouter);
 
 router.use((req, res, next) => {
-  const error = new Error("Not Found");
+  const error = new Error('Not Found');
   error.status = 404;
-  error.message = "Could not find that resource";
+  error.message = 'Could not find that resource';
   next(error);
 });
 
--- server/apiRoutes/venues.js
+++ server/apiRoutes/venues.js
@@ -1,34 +1,43 @@
+const venuesRouter = require('express').Router();
+
 const axios = require('axios');
 const moment = require('moment');
 
-// for converting end and start hours for a venue from military time to regular human time 
+// for converting end and start hours for a venue from military time to regular human time
 const convert = (input) => {
-  return moment(input, 'HH:mm:ss').format('h:mm:ss A');
-}
+  return moment(input, 'HH:mm').format('h:mm A');
+};
 
 // for finding day of the week since yelp uses numbers (e.g Monday = 0, Sunday = 6)
 const dayOfWeek = {
-  0: 'Monday', 
-  1: 'Tuesday', 
-  2: 'Wednesday', 
-  3: 'Thursday', 
-  4: 'Friday', 
-  5: 'Saturday', 
-  6: 'Sunday'
-}
+  0: 'Monday',
+  1: 'Tuesday',
+  2: 'Wednesday',
+  3: 'Thursday',
+  4: 'Friday',
+  5: 'Saturday',
+  6: 'Sunday',
+};
 
 const findDayOfWeek = (num) => {
-  return dayOfWeek[num]
-}
+  return dayOfWeek[num];
+};
 
 //will store in process.env once we figure it out
-const TOKEN =
+let TOKEN =
   '_2xNYMdiNkK47vAXUI6TX4TZEvGdmVjME5dVO-YoXSJgd2N31SfwR0hXUE-Lp5SY6-New20UEpRJdBDgbl3kcD_3cS5q6Rh7xAPajw5lUSiSYKZcqsTSSC0GR2Z-Y3Yx';
 
-const query = `{
-    search(term: "venue", location: "11209", categories: "venues", price: "2") {
+require('dotenv').config();
+
+// const TOKEN = process.env.IRAIS_YELP_TOKEN
+// console.log("process.env.", process.env.IRAIS_YELP_TOKEN)
+
+const queryForAllVenues = `{
+    search(term: "venue", location: "11209", categories: "venues",) {
       total
       business {
+        id
+        alias
         name
         phone
         price
@@ -59,7 +68,38 @@ const query = `{
   }
   `;
 
-const getVenues = async () => {
+const queryForSingleVenue = `{
+      business(id: "g6QOBY2bmEw5CKfiZ43egQ") {
+        id
+        alias
+        name
+        phone
+        price
+        photos
+        url
+        hours {
+          open {
+            is_overnight
+            end
+            start
+            day
+          }
+        }
+        reviews {
+          id
+          text
+          rating
+        }
+        location {
+          address1
+          city
+          state
+          country
+        }
+        rating
+  }
+}`;
+const getVenues = async (queryType) => {
   const options = {
     method: 'POST',
     url: 'https://api.yelp.com/v3/graphql',
@@ -67,12 +107,14 @@ const getVenues = async () => {
       'content-type': 'application/graphql',
       Authorization: `Bearer ${TOKEN}`,
     },
-    data: query,
+    data: queryType === 'single' ? queryForSingleVenue : queryForAllVenues,
   };
-  axios
+  return axios
     .request(options)
     .then(function (response) {
       const res = response.data;
+      console.log('venue', res.data);
+      return res;
     })
 
     .catch(function (error) {
@@ -80,4 +122,27 @@ const getVenues = async () => {
     });
 };
 
-getVenues();
+venuesRouter.post('/', async (req, res, next) => {
+  try {
+    const queryType = 'all';
+    const data = await getVenues(queryType);
+    console.log('DATA', data);
+    res.send(data).status(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+venuesRouter.post('/:id', async (req, res, next) => {
+  // get yelp id from frontend and use singleVenueQuery
+  try {
+    const queryType = 'single';
+    const data = await getVenues(queryType);
+    console.log('DATA', data);
+    res.send(data).status(200);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = venuesRouter;
