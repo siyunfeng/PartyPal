38b4dd463cdcaca2057b98221a31a5d8083f14a6
Siyun Feng <siyunf21@gmail.com>
2022-11-30 00:02:14 -0500

    set up basic UserHome page

--- client/components/UserHome.js
+++ client/components/UserHome.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
-export const UserPage = () => {
+const UserHome = (props) => {
+  const { username, firstName, lastName, email } = props.user;
   return (
-    <h1>Welcome {}</h1>
-  )
-}
+    <div>
+      <div className="user-profile">
+        <div>
+          {/* QUESTION: should the user be able to upload their profile photo? */}
+          <img />
+        </div>
+        <div>
+          <p>Username: {username}</p>
+          <p>Email: {email}</p>
+        </div>
+      </div>
+      <div className="user-home-favorite">
+        <h2>Favorite</h2>
+        <div>Venues</div>
+        <div>Catering</div>
+      </div>
+      <div className="user-home-events">
+        <div>
+          <h2>Events</h2>
+          <button>Create Future Event</button>
+        </div>
+        <div>the latest 3 upcoming events</div>
+      </div>
+    </div>
+  );
+};
 
+const mapState = (state) => {
+  return {
+    user: state.auth,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {};
+};
+
+export default connect(mapState, mapDispatch)(UserHome);
