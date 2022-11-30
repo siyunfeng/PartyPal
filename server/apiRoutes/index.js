5beb10e67e8b7e07e780cfd7b6cbbe0f07077d1e
Jane Yeh <jane.yeh72@gmail.com>
2022-11-30 17:45:38 -0500

    single business search with dummy data

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -1,12 +1,14 @@
-const router = require("express").Router();
-const { userRouter } = require("./users");
+const router = require('express').Router();
+const { userRouter } = require('./users');
+const { singleCatererRouter } = require('./singleCaterer');
 
-router.use("/users", userRouter);
+router.use('/users', userRouter);
+router.use('/caterer', singleCatererRouter);
 
 router.use((req, res, next) => {
-  const error = new Error("Not Found");
+  const error = new Error('Not Found');
   error.status = 404;
-  error.message = "Could not find that resource";
+  error.message = 'Could not find that resource';
   next(error);
 });
 
