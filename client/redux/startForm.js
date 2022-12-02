bfce3f4cf87d116ce0a8b5630ffff24ca7b8e60b
Jane <101992039+j-yeh@users.noreply.github.com>
2022-12-02 09:27:53 -0500

    rm console.log

--- client/redux/startForm.js
+++ client/redux/startForm.js
@@ -23,10 +23,6 @@ export const sendInitialQuery = (initialQuery, history) => {
 const startFormReducer = (state = {}, action) => {
   switch (action.type) {
     case SEND_INITIAL_QUERY:
-      console.log(
-        'startFormReducer - action.initialQuery =',
-        action.initialQuery
-      );
       return action.initialQuery;
     default:
       return state;
