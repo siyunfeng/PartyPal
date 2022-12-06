479cc979567c92cbbf2ee5976998a5ac3772bd17
Irais Valenzuela <iraisvq8@gmail.com>
2022-12-06 17:04:38 -0500

    fixed useStyles bug

--- client/components/Auth.js
+++ client/components/Auth.js
@@ -15,6 +15,26 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 
 export const AuthForm = (props) => {
   const classes = useStyles();
